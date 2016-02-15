2ae55302dafc8ad96ad058ca8f4a549cd5fe3bc1
EGOIST <0x142857@gmail.com>
2016-02-15 23:39:46 +0800

    hide spinner when testing

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ import test from 'ava'
 import execa from 'execa'
 
 test('main', async t => {
-	const {stdout} = await execa.shell('node is-taken whadasd')
+	const {stdout} = await execa.shell('node is-taken whadasd --silent')
 	t.true(stdout.indexOf('Let the hacking begin') > -1)
 })
